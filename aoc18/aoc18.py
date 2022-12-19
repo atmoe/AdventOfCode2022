4440371544059b487ed33a213d5a6e53419f1f00
atmoe <18740986+atmoe@users.noreply.github.com>
2022-12-19 07:08:33 -0600

    Day 18 - full implementation

--- aoc18/aoc18.py
+++ aoc18/aoc18.py
@@ -6,7 +6,8 @@ import re
 assert len(sys.argv) == 2, sys.argv[0] + " requires 1 argument!"
 cubes   = []
 cubeSet = set()
-
+maxPos = [0, 0, 0]
+minPos = [100000, 100000, 100000]
 inputFile = open(sys.argv[1], "r")
 for line in inputFile.readlines():
     cube = [int(x) for x in line.rstrip().split(',')]
@@ -14,6 +15,15 @@ for line in inputFile.readlines():
     cubes.append(cubeTuple)
     cubeSet.add(tuple(cubeTuple))
 
+
+    if cubeTuple[0] > maxPos[0]: maxPos[0] = cubeTuple[0]
+    if cubeTuple[1] > maxPos[1]: maxPos[1] = cubeTuple[1]
+    if cubeTuple[2] > maxPos[2]: maxPos[2] = cubeTuple[2]
+    if cubeTuple[0] < minPos[0]: minPos[0] = cubeTuple[0]
+    if cubeTuple[1] < minPos[1]: minPos[1] = cubeTuple[1]
+    if cubeTuple[2] < minPos[2]: minPos[2] = cubeTuple[2]
+
+print(minPos, maxPos)
 
 print("------------------")
 print("---- PART 1 ------")
@@ -39,6 +49,100 @@ print(freeEdges)
 print("------------------")
 print("---- PART 2 ------")
 print("------------------")
+freeEdges = 0
+outerSet = set()
+innerSet = set()
+for c in cubes:
+    #print('------------')
+    #print('c = ', c)
+    #print('o=', end='')
+    #for o in outerSet:
+    #    print(o, end='')
+    #print()
+    #print('i=', end='')
+    #for i in innerSet:
+    #    print(i, end='')
+    #print()
+
+    dirs = []
+    dirs.append((c[0],   c[1]+1, c[2]))     # up
+    dirs.append((c[0],   c[1]-1, c[2]))     # down
+    dirs.append((c[0],   c[1],   c[2]+1))   # front
+    dirs.append((c[0],   c[1],   c[2]-1))   # back
+    dirs.append((c[0]+1, c[1],   c[2]))     # left
+    dirs.append((c[0]-1, c[1],   c[2]))     # right
+
+    for d in dirs:
+        #print('-', d)
+        if   d in cubeSet:
+            #print('-- is cube')
+            continue
+        elif d in innerSet:
+            #print('-- is inner')
+            continue
+        elif d in outerSet:
+            #print('-- is outer')
+            freeEdges+=1
+            continue
+        else:
+            # unclassified neighbor
+            #print('-- unclassified')
+            potentialSet = set()
+            potentialSet.add(d)
+            nodesToClassify = [d]
+
+            # trace until no neighbors added (inner) or x,y,z go past bounding box (outer)
+            classified = False
+            classification = None
+            while len(nodesToClassify) > 0 and not classified:
+                newNodes = []
+                for n in nodesToClassify:
+                    nDirs = []
+                    nDirs.append((n[0],   n[1]+1, n[2]))     # up
+                    nDirs.append((n[0],   n[1]-1, n[2]))     # down
+                    nDirs.append((n[0],   n[1],   n[2]+1))   # front
+                    nDirs.append((n[0],   n[1],   n[2]-1))   # back
+                    nDirs.append((n[0]+1, n[1],   n[2]))     # left
+                    nDirs.append((n[0]-1, n[1],   n[2]))     # right
+                    for nd in nDirs:
+                        #print('---', nd)
+                        if   nd in cubeSet:
+                            #print('----- cube')
+                            continue
+                        elif   nd in potentialSet:
+                            #print('----- in set already')
+                            continue
+                        elif nd in innerSet:
+                            print('should not be here!')
+                            quit()
+                        elif nd in outerSet:
+                            #print('----- outer(found)')
+                            potentialSet.add(nd) 
+                            classified = True
+                            classification = 'outer'
+                            outerSet = outerSet.union(potentialSet)
+                        elif nd[0] > maxPos[0] or nd[0] < minPos[0] or \
+                             nd[1] > maxPos[1] or nd[1] < minPos[1] or \
+                             nd[2] > maxPos[2] or nd[2] < minPos[2]:
+                            #print('----- outer(range)')
+                            potentialSet.add(nd) 
+                            classified = True
+                            classification = 'outer'
+                            outerSet = outerSet.union(potentialSet)
+                        else:
+                            #print('----- unclassified')
+                            potentialSet.add(nd) 
+                            newNodes.append(nd)
+
+                nodesToClassify = newNodes
+
+            if not classified:
+                innerSet = innerSet.union(potentialSet)
+            if classification == 'outer':
+                freeEdges+=1
+
+print(freeEdges)
+
 
 
 
